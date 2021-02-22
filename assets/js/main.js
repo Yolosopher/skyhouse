3ced62312f11a19a58b4fabce9c42482f764545d
Yolosopher <nika.nishnianidze.10@gmail.com>
2021-02-23 03:13:32 +0400

    homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 const main = document.querySelector('.main')
+const fullscreen = document.querySelector('.fullscreen')
+const loadhiddens = document.querySelectorAll('.load-hidden')
+const forloads = document.querySelectorAll('.for-load')
 const header = document.querySelector('.header')
 const goLeftBtn = document.querySelector('.go-nav-left')
 const goRightBtn = document.querySelector('.go-nav-right')
 const mainSliderWrapper = document.querySelector(
 	'.main__slider .swiper-wrapper'
 )
+
+const burger = document.getElementById('burger')
+const respomenu = document.getElementById('respomenu')
 const matchmadia = window.matchMedia('(min-width: 1024px)')
 const respoMainSlider = new Swiper('.respo-main-slider__container', {
 	// init: false,
@@ -24,6 +30,64 @@ const respoMainSlider = new Swiper('.respo-main-slider__container', {
 		el: '.swiper-paginationR',
 	},
 })
+const projectsSlider = new Swiper('.projects-slider', {
+	// init: false,
+	slideToClickedSlide: true,
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	grabCursor: true,
+	// loop: true,
+	// autoplay: {
+	// 	delay: 3500,
+	// 	disableOnInteraction: true,
+	// },
+	keyboard: {
+		enabled: true,
+	},
+	pagination: {
+		el: '.projectSliderPagination',
+	},
+	breakpoints: {
+		1025: {
+			spaceBetween: 25
+		}
+	}
+})
+const partnerssSlider = new Swiper('.partners_slider', {
+	slideToClickedSlide: true,
+	slidesPerView: 3,
+	grabCursor: true,
+	spaceBetween: 25,
+	loop: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: true,
+	},
+	keyboard: {
+		enabled: true,
+	},
+	pagination: {
+		el: '.partners__content__logos__pagination',
+	},
+})
+const partnerssSliderRespo = new Swiper('.partners-sliderRespo', {
+	slideToClickedSlide: true,
+	slidesPerView: 'auto',
+	grabCursor: true,
+	spaceBetween: 10,
+	loop: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: true,
+	},
+	keyboard: {
+		enabled: true,
+	},
+	pagination: {
+		el: '.partners__content__logosRespo__pagination',
+	},
+})
+
 respoMainSlider.slideNext()
 if (!matchmadia) {
 	// respoMainSlider.init()
@@ -55,6 +119,7 @@ const headerBotFixer = (headerMinSize) => {
 		}
 	}
 }
+
 const aboutImgHeightFix = () => {
 	const img = document.querySelector('.img-outter img')
 	const offWidth = img.offsetWidth
@@ -88,18 +153,36 @@ const mainSliderRight = new Swiper('.sw-cntr-right', {
 	slidesPerView: 1,
 	spaceBetween: 25,
 	grabCursor: true,
-	on: {
-		slideChangeTransitionEnd: () => {
-			// console.log('haha')
-		},
-	},
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-paginationRightt',
 	},
 	thumbs: {
 		swiper: mainSliderLeft,
 	},
 })
 window.addEventListener('load', () => {
+	main.classList.add('loaded')
+	setTimeout(() => {
+		fullscreen.classList.add('hidden')
+		loadhiddens.forEach(loadhidden => {
+			loadhidden.classList.remove('load-hidden')
+		})
+	}, 801)
+	setTimeout(() => {
+		forloads.forEach(forload => {
+			forload.classList.add('loaded')
+		})
+	}, 800)
+	setTimeout(() => {
+		forloads.forEach(forload => {
+			forload.classList.remove('loaded')
+			forload.classList.remove('for-load')
+			// main.classList.remove('loaded')
+		})
+	}, 2400);
 	aboutImgHeightFix()
 })
+burger.addEventListener('click', () => {
+	respomenu.classList.toggle('active')
+})
+
